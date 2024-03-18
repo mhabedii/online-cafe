505556f9ef76cd8903180b51bc4189dfcfb3c529
MohammaD <91623835+mohammadhas@users.noreply.github.com>
2024-03-19 00:56:26 +0330

    user/product ui-ux design added

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 
 con = sqlite3.connect('cafe.db')
 cur = con.cursor()
-# cur.execute('''CREATE TABLE IF NOT EXISTS product (
-#     id      NUMERIC PRIMARY KEY,
-#     name    TEXT    NOT NULL,
-#     price   NUMERIC NOT NULL,
-#     picture TEXT
-# );''')
+
+if __name__ == '__main__':
+    app.run()
